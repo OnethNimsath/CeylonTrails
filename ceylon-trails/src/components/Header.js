37ef788ba772433fc60a3ed227d7e21f8d6baca6
Nilantha Sanjeewa <nilanthasanjeewa101@gmail.com>
2025-10-12 17:19:22 +0530

    About us page created.

--- ceylon-trails/src/components/Header.js
+++ ceylon-trails/src/components/Header.js
@@ -1,57 +1,56 @@
-// src/components/Header.js
 import React from 'react';
-import ceylonLogo from '../images/Ceylon-Trails.jpg'; // Make sure the path is correct
+import { Link } from 'react-router-dom';
+import ceylonLogo from '../images/Ceylon-Trails.jpg'; 
 
 const Header = () => {
   return (
     <header className="p-4 bg-white shadow-sm sticky top-0 z-50">
       <nav className="container mx-auto flex justify-between items-center">
-        {/* Logo Container */}
         <div className="flex-shrink-0">
-          <a href="/">
+          {/* 2. Change <a> to <Link> and href to "to" */}
+          <Link to="/">
             <img 
               src={ceylonLogo} 
               alt="Ceylon Journeys Logo" 
               className="h-20 w-auto" // Increased the height
             />
-          </a>
+          </Link>
         </div>
         
-        {/* Navigation Links with Animations */}
         <ul className="hidden md:flex space-x-6 text-lg items-center">
           <li>
-            <a href="/" className="relative group hover:text-[#6a9772] transition-colors duration-300">
+            {/* 3. Update all links to use the Link component and correct paths */}
+            <Link to="/" className="relative group hover:text-[#6a9772] transition-colors duration-300">
               Home
               <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#6a9772] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="relative group hover:text-[#6a9772] transition-colors duration-300">
+            <Link to="/about" className="relative group hover:text-[#6a9772] transition-colors duration-300">
               About 
               <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#6a9772] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="relative group hover:text-[#6a9772] transition-colors duration-300">
+            <Link to="/destinations" className="relative group hover:text-[#6a9772] transition-colors duration-300">
               Destinations
               <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#6a9772] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="relative group hover:text-[#6a9772] transition-colors duration-300">
+            <Link to="/tours" className="relative group hover:text-[#6a9772] transition-colors duration-300">
               Tours
               <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#6a9772] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="relative group hover:text-[#6a9772] transition-colors duration-300">
+            <Link to="/contact" className="relative group hover:text-[#6a9772] transition-colors duration-300">
               Contact
               <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#6a9772] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
-            </a>
+            </Link>
           </li>
         </ul>
         
-        {/* Book Now Button */}
         <button className="bg-[#6a9772] text-white px-4 py-2 rounded-full hover:bg-[#3a6042] transition-all duration-300 hover:scale-105">
         Book Now
         </button>
